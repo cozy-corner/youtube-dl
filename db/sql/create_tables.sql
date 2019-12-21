b64df6ef56bd0b4f2293c8afb2147106a2b6d8ec
cozy <sasakicozy@gmail.com>
2019-12-21 16:04:29 +0900

    create table のゴミを削除

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -9,7 +9,7 @@ create table IF not exists `youtube`
  `title`            TEXT NOT NULL,
  `description`      TEXT NOT NULL,
  `channel_id`       VARCHAR(255) NOT NULL,
- you`publishedAt`      Datetime DEFAULT NULL,
+ `publishedAt`      Datetime DEFAULT NULL,
  `created_at`       Datetime DEFAULT NULL,
  `updated_at`       Datetime DEFAULT NULL,
     PRIMARY KEY (`id`)
