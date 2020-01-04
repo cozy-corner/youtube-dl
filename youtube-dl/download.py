1b8024994a772b58800051cbf273389ed702cc7d
cozy <sasakicozy@gmail.com>
2020-01-04 16:17:31 +0900

    requirements.txt を分離。複数ダウンロード対応

--- youtube-dl/download.py
+++ youtube-dl/download.py
@@ -3,25 +3,27 @@ from  db import query
 import pandas as pd
 import youtube_dl
 
-def download():
-
-    videos = query.select_all()
+def download(videos):
 
     base_url = 'https://www.youtube.com/watch?v='
 
     # 240p
     kaizodo = '133'
 
-    url = base_url + videos['videoId'].values[0]
+    videos = videos[['videoId', 'title']]
 
-    outtmpl = videos['title'].values[0] + '.%(ext)s'
-    ydl = youtube_dl.YoutubeDL({'outtmpl': outtmpl,'format': kaizodo})
+    for index, video in videos.iterrows():
 
-    with ydl:
-        result = ydl.extract_info(
-            url,
-            download=True # We just want to extract the info
-        )
-    return result
+        url = base_url + video['videoId']
 
-download()
+        outtmpl = video['title'] + '.%(ext)s'
+        ydl = youtube_dl.YoutubeDL({'outtmpl': outtmpl,'format': kaizodo})
+
+        with ydl:
+            ydl.extract_info(
+                url,
+                download=True # We just want to extract the info
+            )
+
+videos = query.select_all()
+download(videos)
